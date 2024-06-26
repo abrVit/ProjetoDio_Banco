aeec8037e5a5ec2b2133612f9843507e2bdeb72a
abrVit <joaovtdeabreu@gmail.com>
2024-06-25 23:03:15 -0300

    Alteração de código, adicionando mensagem final

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -13,6 +13,7 @@ public class ContaTerminal{
 
         System.out.print("Digite seu nome: ");
         nomeCliente = sc.next();
+        sc.nextLine();
 
         System.out.print("Digite sua agencia: ");
         agencia = sc.next();
@@ -22,6 +23,10 @@ public class ContaTerminal{
 
         System.out.println("Digite seu saldo: ");
         saldo = sc.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, agencia, numeroConta, saldo);
+
+        sc.close();
     }
 
 }
