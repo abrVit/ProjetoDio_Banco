500b16b7c7c473b4be102ba23a9edc19de9067b9
abrVit <joaovtdeabreu@gmail.com>
2024-06-30 19:03:57 -0300

    Alteração de IDE

---
+++ JavaDio/src/application/ExemploFor_Array.java
@@ -0,0 +1,17 @@
+package application;
+
+public class ExemploFor_Array {
+
+    public static void main(String[] args) {
+        
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        System.out.println(alunos.length);
+
+        for (int i = 0; i < alunos.length; i++){
+            System.out.println(alunos[i]);
+        }
+
+    }
+
+}
